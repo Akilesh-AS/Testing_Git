5ff5e32556027a78a8ea6d0fca951d174c4e15cc
Akilesh-AS <asakilesh8@gmail.com>
2024-02-12 15:42:10 +0530

    Added the function to find the Sum of numbers...

---
+++ Sum.py
@@ -0,0 +1,10 @@
+def sum(x, y):
+     return x+y
+
+num1 = int(input("Enter the number 1 : "))
+num2 = int(input("Enter the number 2 : "))
+
+result = sum(num1, num2)
+
+print(f"Sum of the number {num1} and {num2} is {result}.")
+ 
